fc5724ece62c5884986343788cf28b715b9a1f8a
gabi <gabriellabenevides22@gmail.com>
2025-06-18 19:56:00 -0300

    Adiciona Swagger UI e configurações de inicialização
    Inclui o pacote `Swashbuckle.AspNetCore.SwaggerUI` na versão `9.0.1` em múltiplos projetos para documentação da API.
    Atualiza `Program.cs` para habilitar o Swagger e suas rotas.
    Altera `launchSettings.json` para abrir o navegador automaticamente ao iniciar a aplicação.

--- GumpFinancaAPI/GumpFinancaAPI/Program.cs
+++ GumpFinancaAPI/GumpFinancaAPI/Program.cs
@@ -1,4 +1,6 @@
 using GumpFinanca.MySql.Context;
+using GumpFinanca.MySql.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,5 +12,16 @@ builder.Services.AddDbContext<MySqlContext>(options =>
 
 var app = builder.Build();
 
+
+await app.RunAsync();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Financa API V1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.MapControllers();
 
 await app.RunAsync();
